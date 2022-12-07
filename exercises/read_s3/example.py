cdb40a7eb9388326b0ea2a95ff716249e9614ead
alexandremontrieux <alexandre.montrieux@gmail.com>
2022-12-07 10:24:59 +0000

    fileimport

--- exercises/read_s3/example.py
+++ exercises/read_s3/example.py
@@ -2,10 +2,10 @@ from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
 BUCKET = "dmacademy-course-assets"
-KEY = ""
+KEY = "airflow/data.csv"
 
 config = {
-    "spark.jars.packages": "org.apache.hadoop:hadoop-aws:",
+    "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
     "spark.hadoop.fs.s3a.aws.credentials.provider": "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
 }
 conf = SparkConf().setAll(config.items())
